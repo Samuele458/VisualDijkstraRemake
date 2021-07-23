9532db821e4909fc876379391f03f33cdc20302f
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-07-23 13:45:04 +0200

    init NodeState and GraphState

---
+++ VisualDijkstraRemake/Models/GraphState.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace VisualDijkstraRemake.Models
+{
+    class GraphState
+    {
+
+        public string Source { get; set; }
+
+        public string Dest { get; set; }
+
+        public string Message { get; set; }
+
+        private List<NodeState> _nodesStates;
+
+        public GraphState(List<Node> nodes)
+        {
+            setNodes(nodes);
+        }
+
+        public GraphState(Graph graph) : this(graph.Nodes)
+        {
+
+        }
+
+        public void setNodes(List<Node> nodes)
+        {
+            _nodesStates = nodes.ConvertAll(
+                new System.Converter<Node, NodeState>(
+                    (node) => new NodeState(node.Name)
+                )
+            );
+        }
+
+        public void setDistance(string nodeName, int distance)
+        {
+            NodeState node = _nodesStates.Find(n => n.Name == nodeName);
+            if (node != null)
+            {
+                node.Distance = distance;
+            }
+        }
+
+        public void setProcessed(string nodeName, bool processed)
+        {
+            NodeState node = _nodesStates.Find(n => n.Name == nodeName);
+            if (node != null)
+            {
+                node.Processed = processed;
+            }
+        }
+
+        public void setPrevious(string nodeName, string previous)
+        {
+            NodeState node = _nodesStates.Find(n => n.Name == nodeName);
+            if (node != null)
+            {
+                node.Previous = previous;
+            }
+        }
+    }
+}
---
+++ VisualDijkstraRemake/Models/NodeState.cs
@@ -0,0 +1,30 @@
+﻿namespace VisualDijkstraRemake.Models
+{
+    class NodeState
+    {
+        public const int INF = 999999999;
+
+        public string Name { get; set; }
+
+        public string Previous { get; set; }
+
+        public int Distance { get; set; }
+
+        public bool Processed { get; set; }
+
+
+        public NodeState(
+                    string name = "DEFAULT_NODE",
+                    int distance = INF,
+                    string previous = "DEFAULT_PREVIOUS_NODE",
+                    bool processed = false)
+        {
+            Name = name;
+            Distance = distance;
+            Previous = previous;
+            Processed = processed;
+        }
+    }
+
+
+}
