c8b95d46f88f4c3c3a990e514c0a3623add43c69
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-08-26 00:26:12 +0200

    docs

--- src/WebApp/Models/User.cs
+++ src/WebApp/Models/User.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace WebApp.Models
 {
+    /// <summary>
+    ///  Users handling model
+    /// </summary>
     public class User
     {
         public int Id { get; set; }
@@ -14,6 +17,9 @@ namespace WebApp.Models
         [JsonIgnore]
         public string Password { get; set; }
 
+        /// <summary>
+        ///  Graphs owned by user
+        /// </summary>
         public virtual IList<GraphModel> Graphs { get; set; }
 
         public User()
