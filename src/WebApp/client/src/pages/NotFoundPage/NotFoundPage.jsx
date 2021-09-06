a5b0c285f774db38ce3d5c3f891efb0c368fe679
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-09-05 17:30:50 -0700

    404 not found page

--- src/WebApp/client/src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/WebApp/client/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,19 @@
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const NotFoundPage = () => {
-  return <h1>404 not found</h1>;
+  return (
+    <div className="not-found-page-wrapper">
+      <div className="not-found-page">
+        <div className="vflex flex-align-center">
+          <FontAwesomeIcon icon={faExclamationTriangle} className="text-7" />
+          <h1 className="text-6">404</h1>
+          <h2 className="text-2">Not Found</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
